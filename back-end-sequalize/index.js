af1ef9b2775d433b62086a66bb305067678a7ecc
Ankit kumar <ankit.kumar@creolestudios.com>
2022-07-26 18:39:16 +0530

    sequilize connect

--- back-end-sequalize/index.js
+++ back-end-sequalize/index.js
@@ -1,6 +1,14 @@
+const express = require("express");
+const sequelize = require("./config/connection");
 const app = require("./server/server.js");
 const dotenv = require("dotenv");
 dotenv.config();
-app.listen(process.env.PORT, () => {
-  console.log(`server listen port ${process.env.PORT}`);
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+sequelize.sync({ force: false }).then((res) => {
+  app.listen(process.env.PORT, () => {
+    console.log(`server listen port ${process.env.PORT},${res}`);
+  });
 });
